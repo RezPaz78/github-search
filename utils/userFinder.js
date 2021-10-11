a679e2eceea895a345b9aa6683b09a4c290a07aa
Reza Pazan <rezapazan@rocketmail.com>
2021-10-12 00:28:22 +0330

    removed some personal info :))

--- utils/userFinder.js
+++ utils/userFinder.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 const REPOS_SORT = "updated:acs";
-// const CLIENT_ID = "b4c3b0878a5d68e6bc47";
-// const CLIENT_SECRET = "77da679b0efc6c3291296495cea14632c02b84a4";
 
 export const getUser = async (username) => {
   const resProfile = await axios.get(
