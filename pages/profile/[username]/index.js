4640458e0a1926f6ce29f7f83d569b9216653cdf
reza pazan <rezapazan@rocketmail.com>
2021-10-12 13:52:32 +0330

    removed some unused code

--- pages/profile/[username]/index.js
+++ pages/profile/[username]/index.js
@@ -1,11 +1,9 @@
-import axios from "axios";
 import { useRouter } from "next/router";
 import Meta from "../../../components/Meta";
 import { getUser } from "../../../utils/userFinder";
-import Error from "../../../components/Error";
 import Profile from "../../../components/Profile";
 
-const User = ({ profile, repos, message }) => {
+const User = ({ profile, repos }) => {
   const router = useRouter();
   const { username } = router.query;
   
@@ -35,50 +33,5 @@ export const getServerSideProps = async (context) => {
     };
   }
 };
-
-// export const getStaticPaths = async () => {
-//   const res = await axios.get(`https://api.github.com/users`);
-
-//   const users = res.data;
-
-//   const usernames = users.map((user) => user.login);
-
-//   const paths = usernames.map((username) => ({
-//     params: { username: username },
-//   }));
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
-
-// export const getStaticProps = async (context) => {
-//   const res = await axios.get(
-//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-//   );
-
-//   const article = res.data;
-
-//   return {
-//     props: {
-//       article,
-//     },
-//   };
-// };
-
-// export const getStaticPaths = async () => {
-//   const res = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
-
-//   const articles = res.data;
-
-//   const ids = articles.map((article) => article.id);
-//   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
 
 export default User;
